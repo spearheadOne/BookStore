92963565aec3a2f92c0015993308848044b34638
Alex Bondar <abondar1992@gmail.com>
2026-09-22 00:05:46 +0200

    initial setup

---
+++ book-service/src/main/java/org/abondar/experimental/bookStore/BookServiceApplication.java
@@ -0,0 +1,12 @@
+package org.abondar.experimental.bookStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookServiceApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(BookServiceApplication.class, args);
+    }
+}
